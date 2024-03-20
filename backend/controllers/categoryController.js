79e688d00a4e25050a974664b1789461773a7b39
patilvivek643 <vivekpatil642003@gmail.com>
2024-03-20 12:43:49 +0530

    worked on user controller category

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -8,13 +8,10 @@ const createCategory = asyncHandler(async (req, res) => {
     if (!name) {
       return res.json({ error: "Name is required" });
     }
-
     const existingCategory = await Category.findOne({ name });
-
     if (existingCategory) {
       return res.json({ error: "Already exists" });
     }
-
     const category = await new Category({ name }).save();
     res.json(category);
   } catch (error) {
@@ -27,9 +24,7 @@ const updateCategory = asyncHandler(async (req, res) => {
   try {
     const { name } = req.body;
     const { categoryId } = req.params;
-
     const category = await Category.findOne({ _id: categoryId });
-
     if (!category) {
       return res.status(404).json({ error: "Category not found" });
     }
